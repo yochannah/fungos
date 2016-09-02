45d0b791e1b7240807baa998a184070e77f20829
Yo Yehudi <yochannah@gmail.com>
2016-09-02 12:24:02 +0100

    move to most recent reagent and reframe before everything gets out of hand

--- src/cljs/fungos/subs.cljs
+++ src/cljs/fungos/subs.cljs
@@ -2,12 +2,7 @@
     (:require-macros [reagent.ratom :refer [reaction]])
     (:require [re-frame.core :as re-frame]))
 
-(re-frame/reg-sub
- :name
- (fn [db]
-   (:name db)))
-
-  (re-frame/register-sub
+  (re-frame/reg-sub-raw
   :organisms
   (fn [db]
    (reaction (:organisms @db))))
